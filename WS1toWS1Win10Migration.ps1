558e1e213c54bb247c150150b5ab6a9701eacb84
Phil Helmling <helmlingp@vmware.com>
2022-10-20 08:42:56 +1030

    changed cleanup as causing rerun

--- WS1toWS1Win10Migration.ps1
+++ WS1toWS1Win10Migration.ps1
@@ -288,9 +288,9 @@ function Invoke-Cleanup {
     }
 
     #Remove Task that started the migration
-    Unregister-ScheduledTask -TaskName "WS1Win10Migration" -Confirm:$false
+    Unregister-ScheduledTask -TaskName "$scriptBaseName" -Confirm:$false
     #Remove folder containing scripts and agent file
-    Remove-Item -Path $current_path -Recurse -Force
+    #Remove-Item -Path $current_path -Recurse -Force
 }
 
 function disable-notifications {
